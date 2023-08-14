eff3b3cc5f820c87ced07c9738252757518cc105
mapacarta <omergunduz123@hotmail.com>
2023-08-14 09:37:35 +0300

    gsap, curve added

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,8 @@
-import SpriteMask from "./utils/spriteMask";
+import SpriteMask from "./spriteMask";
+// import { Curves, Math } from "phaser";
+import { Curve } from "./curve-2d/Curve";
+import { Vector2 } from "./curve-2d/Vector2";
+import gsap, { Power0 } from "gsap";
 
 class Utils {
 	constructor(pixiScene) {
@@ -7,6 +11,26 @@ class Utils {
 	spriteMask(sprite, mask, invertAlpha) {
 		new SpriteMask(sprite, mask, invertAlpha);
 	}
-	curve() {}
+	curve(startPoint, endPoint, controlPoint, duration, callback) {
+		let path = { t: 0, vec: new Vector2() };
+
+		var sp = new Vector2(startPoint.x, startPoint.y);
+		var ep = new Vector2(endPoint.x, endPoint.y);
+		var cp = new Vector2(controlPoint.x, controlPoint.y);
+		let curve = new Curve.QuadraticBezier(sp, cp, ep);
+
+		gsap.to(path, {
+			t: 1,
+			duration,
+			onUpdate: () => {
+				curve.getPoint(path.t, path.vec);
+				let x = path.vec.x;
+				let y = path.vec.y;
+				callback(x, y);
+			},
+			onComplete: () => {},
+			ease: Power0.easeNone,
+		});
+	}
 }
 export default Utils;
