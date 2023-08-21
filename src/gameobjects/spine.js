c5c59244b2d55da1242691eecf72be33caa66272
Cem Kayaturk <cemkayaturk@gmail.com>
2023-08-21 13:03:12 +0300

    spine update 21/8/2023
    setSkinByName() fixed
    
    play() onComplete callback added
    
    animNames skinNames getters added
    
    resetSkin removeAnimation methods added

--- src/gameobjects/spine.js
+++ src/gameobjects/spine.js
@@ -15,31 +15,50 @@ class PfSpine extends GameObject {
 		super(pixiObj, x, y);
 		pixiObj.gameObject = this;
 
-		skinName = skinName || "default";
-		this.setSkinByName(skinName);
+		this.skinName = skinName || "default";
+		this.setSkinByName(this.skinName);
 
 		this.spineName = spineName;
-
+	
 		if (animName) {
 			this.play(animName, loop);
 		}
-
+		
 		this.isSpine = true;
 		this.type = objectTypes.SPINE;
 	}
 
 	setSkinByName(skinName) {
+		this.pixiObj.skeleton.setSkin(null);
 		this.pixiObj.skeleton.setSkinByName(skinName);
+		this.pixiObj.skeleton.setSlotsToSetupPose();
 		this.baseWidth = this.pixiObj.width;
 		this.baseHeight = this.pixiObj.height;
 		this.skinName = skinName;
 		return this;
 	}
-	play(animName, loop) {
+
+	removeSkin() {
+		this.skinName = null;
+		this.pixiObj.state.clearTracks();
+		this.pixiObj.skeleton.setSkin(null);
+		this.pixiObj.skeleton.setSlotsToSetupPose();
+		return this;
+	}
+
+	play(animName, loop, callback) {
 		this.pixiObj.state.setAnimation(0, animName, loop);
+		this.pixiObj.state.tracks[0].listener = {
+			complete: callback,
+		};
 		this.animName = animName;
 		this.loop = loop;
 		return this;
+	}
+
+	resetAnimation() {
+		this.pixiObj.state.clearTracks();
+		this.pixiObj.state.setEmptyAnimation(0, 0);
 	}
 
 	set timescale(value) {
@@ -47,6 +66,30 @@ class PfSpine extends GameObject {
 	}
 	get timescale() {
 		return this.pixiObj.state.timeScale;
+	}
+
+	get skinNames() {
+		return this.pixiObj.spineData.skins.map((skin) => skin.name);
+	}
+
+	get animNames() {
+		return this.pixiObj.spineData.animations.map((anim) => anim.name);
+	}
+
+	set onComplete(callback) {
+		this.pixiObj.state.tracks[0].listener.complete = callback;
+	}
+
+	addListener(props = {
+		event: function(entry, event) { console.log('event fired '+event.data+' at track' + entry.trackIndex) },
+		complete: function(entry) { console.log('track '+entry.trackIndex+' completed '+entry.loopsCount()+' times') },
+		start: function(entry) { console.log('animation is set at '+entry.trackIndex) },
+		end: function(entry) { console.log('animation was ended at '+entry.trackIndex) },
+		dispose: function(entry) { console.log('animation was disposed at '+entry.trackIndex) },
+		interrupted: function(entry) { console.log('animation was interrupted at '+entry.trackIndex) }
+	}) {
+		this.pixiObj.state.addListener(props);
+		this.listener = this.pixiObj.state.listeners[0]
 	}
 
 	setTimeScale(value) {
